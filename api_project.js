c91009055d02dcde199f6ce673860dee1669213b
getbubun <getbubun@gmail.com>
2019-04-30 22:22:30 +0530

    docs updated

---
+++ api_project.js
@@ -0,0 +1,14 @@
+define({
+  "name": "ecommerce-api",
+  "version": "1.0.0",
+  "description": "This is a demo REST API for example ecom platform  ",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-04-16T17:40:06.449Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
